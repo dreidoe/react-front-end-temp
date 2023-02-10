cc0dcb7fad20c59f4f31ac1842ce4b3d00b7da7f
Aundrei Bryant <drei97@gmail.com>
2023-02-10 10:08:43 -0600

    packages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import "./App.css";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="App">
+      <h1>Calendar</h1>
+    </div>
+  );
 }
 
 export default App;
